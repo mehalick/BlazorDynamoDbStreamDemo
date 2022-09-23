bc56aa10cfddb7738f5474e90f5bdd256967da17
Andy Mehalick <andy.mehalick@outlook.com>
2022-09-23 12:00:29 -0400

    initial commit

---
+++ BlazorDynamoDbStreamDemo/Server/Program.cs
@@ -0,0 +1,37 @@
+namespace BlazorDynamoDbStreamDemo.Server;
+
+public class Program
+{
+	public static void Main(string[] args)
+	{
+		var builder = WebApplication.CreateBuilder(args);
+
+		builder.Services.AddControllersWithViews();
+		builder.Services.AddRazorPages();
+
+		var app = builder.Build();
+
+		if (app.Environment.IsDevelopment())
+		{
+			app.UseWebAssemblyDebugging();
+		}
+		else
+		{
+			app.UseExceptionHandler("/Error");
+			app.UseHsts();
+		}
+
+		app.UseHttpsRedirection();
+
+		app.UseBlazorFrameworkFiles();
+		app.UseStaticFiles();
+
+		app.UseRouting();
+
+		app.MapRazorPages();
+		app.MapControllers();
+		app.MapFallbackToFile("index.html");
+
+		app.Run();
+	}
+}
